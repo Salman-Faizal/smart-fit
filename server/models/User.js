13a8bd213bc51a1710cce84a202db0be404df3ea
Salman-Faizal <salmanfaizal1000@gmail.com>
2026-03-31 01:52:20 +0530

    feat(auth): add login UI, create User model, implement bcryptjs password hashing, add /register route

---
+++ server/models/User.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+const userSchema = new mongoose.Schema(
+  {
+    name: String,
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 6,
+    },
+    role: {
+      type: String,
+      enum: ["customer", "admin"],
+      default: "customer",
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+// hooks
+userSchema.pre("save", async function () {
+  if (!this.isModified("password")) return;
+  this.password = await bcrypt.hash(this.password, 10);
+});
+
+//methods
+userSchema.methods.comparePassword = async function (enteredPasswrd) {
+  return await bcrypt.compare(enteredPasswrd, this.password);
+};
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
--- server/server.js
+++ server/server.js
@@ -1,14 +1,36 @@
 const express = require("express");
-const mongoose = require("mongoose");
 const connectDB = require("./config/db");
+const User = require("./models/User");
 
 const app = express();
 const PORT = 3000;
+
+app.use(express.json());
 
 connectDB();
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.post("/register", async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    const user = await User.create({
+      name,
+      email,
+      password,
+    });
+
+    res.status(201).json({
+      message: "User Registered Successfully",
+    });
+  } catch (err) {
+    res.status(500).json({
+      message: "Failed to create user",
+      err,
+    });
+  }
 });
 
 app.listen(PORT, () => {
